25b86d7bf69f5798922a20700af4369d237e0f85
Andrey Voroshnin <96011822+TaurusAlpha@users.noreply.github.com>
2024-04-07 12:34:52 +0300

    update logging

--- lambda-cloudwatch-log-default.py
+++ lambda-cloudwatch-log-default.py
@@ -7,21 +7,21 @@ if TYPE_CHECKING:
     from mypy_boto3_logs import CloudWatchLogsClient
 
 # Configure logging
+logging.basicConfig(format="%(asctime)s %(message)s",level=logging.getLevelName(os.getenv("logger_level", "DEBUG")))
 logger = logging.getLogger()
-logging.basicConfig(format="%(asctime)s %(message)s")
-logger.setLevel(logging.INFO if os.getenv("logger_level") else logging.DEBUG)
 
 
 def lambda_handler(event, context) -> None:
     """
     Lambda function handler for setting default retention policy for CloudWatch log groups.
     """
-    log_client: CloudWatchLogsClient = boto3.client("logs")
-    default_log_retention = os.environ.get("default_log_retention", 365)
+    logger.info(f"Recieved event: {event}")
+    log_client: CloudWatchLogsClient = boto3.client("logs", region_name=os.getenv("AWS_REGION"))
+    default_log_retention = int(os.getenv("default_log_retention", 365))
     log_group_name = event["requestParameters"]["logGroupName"]
 
     if "retentionInDays" not in event["requestParameters"]:
         logger.info(f"Setting default retention policy for log group: {log_group_name}")
         log_client.put_retention_policy(
-            logGroupName=log_group_name, retentionInDays=int(default_log_retention)
+            logGroupName=log_group_name, retentionInDays=default_log_retention
         )
